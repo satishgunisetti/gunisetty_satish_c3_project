ae24e7f99094fe214cc7e1ed2b36c75236375b4b
Satish Gunisetty <satish_gunisetty@epam.com>
2021-09-23 17:18:21 +0530

    Part 2: Solution

--- src/main/java/RestaurantService.java
+++ src/main/java/RestaurantService.java
@@ -1,13 +1,15 @@
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class RestaurantService {
     private static List<Restaurant> restaurants = new ArrayList<>();
 
-    public Restaurant findRestaurantByName(String restaurantName){
-        return null;
-        //DELETE ABOVE STATEMENT AND WRITE CODE HERE
+    public Restaurant findRestaurantByName(String restaurantName) throws restaurantNotFoundException {
+        Optional<Restaurant> searchedRestaurantOpt = restaurants.stream().filter(restaurant -> restaurant.getName().equals(restaurantName)).findAny();
+        Restaurant searchedRestaurant = searchedRestaurantOpt.orElseThrow(() -> new restaurantNotFoundException("No Restaurant available with the name: " + restaurantName + "."));
+        return searchedRestaurant;
     }
 
 
